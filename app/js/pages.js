0a645e8bbdf18d2b32af9237b45fc812e6341c02
joeyklee <josephkanglee@gmail.com>
2016-06-03 13:27:09 -0700

    added toggle page functionality

--- app/js/pages.js
+++ app/js/pages.js
@@ -1,32 +1,45 @@
 var app = app || {};
 
-app.pages = (function(){
-	// create empty el to take on attributes of the main el
-	var el = null
+app.pages = (function() {
+    // create empty el to take on attributes of the main el
+    var el = null
 
 
-	function sayHello(){
-		console.log("hello");
-	}
+    function sayHello() {
+        console.log("hello");
+    }
 
-	function displaySelectedPage(domid){
-		var pagelist = []
-
-		$(domid).click(function(){
-			$('#cartodb-map').css("display","none");
-
-			
-		})
-	}
+    function displaySelectedPage() {
+        var pagelist = ["about", "current-research", "contact"];
+        var pageClass, pageId, buttonName;
 
 
-	function init(){
-		sayHello();
-		displaySelectedPage("#about");
-	}
+        $(".nav li a").click(function() {
+        	// set the display of the map to none
+        	$('#cartodb-map').css("display", "none");
+            // console.log($(this)[0].id);
+            // get the name of the button clicked
+            buttonName = $(this)[0].id;
+            // set the page class and id which match in the html file
+            pageClass = "." + buttonName;
+            pageId = "#" + buttonName;
+            // run through the list of pages and make visible that which was selected
+            pagelist.forEach(function(d) {
+                $("."+d).css("display", "none");
+                if (buttonName == d) {
+                    $(pageClass).css("display", "block");
+                }
+            });
+        });
+    }
 
-	return {
-		init: init
-	}
+
+    function init() {
+        sayHello();
+        displaySelectedPage();
+    }
+
+    return {
+        init: init
+    }
 })();
-
