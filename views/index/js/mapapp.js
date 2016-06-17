7c97b266378798e8a519aced5135a8ac7bd8a776
joeyklee <jozo@dhcp-128-189-202-221.ubcsecure.wireless.ubc.ca>
2016-06-17 15:59:14 -0700

    test - getting data from https and using sublayers after

--- views/index/js/mapapp.js
+++ views/index/js/mapapp.js
@@ -53,40 +53,46 @@ app.mapapp = (function() {
 
     var initCarto = function() {
         console.log("hello");
-        cartodb.createLayer(el.map, {
-            user_name: el.username,
-            type: 'cartodb',
-            https:true,
-            sublayers: [{
-                sql: "SELECT * FROM bgcv10beta_200m_wgs84",
-                cartocss: el.bec_cartocss.zone,
-                interactivity: "cartodb_id, map_label"
-            }]
-        }).addTo(el.map).done(function(layer) {
-            el.data_layer = layer.getSubLayer(0);
-            el.data_layer.setInteraction(true);
+        cartodb.createLayer(el.map, 'https://becexplorer.cartodb.com/api/v2/viz/f1dab0f4-29b3-11e6-9b74-0ef7f98ade21/viz.json')
+            .addTo(el.map).done(function(layer) {
 
-            layer.on('featureOver', function(e, latlng, pos, data, subLayerIndex) {
+                console.log(layer);
+
+                el.data_layer = layer;
+
+                // change the query for the first layer
+                  var subLayerOptions = {
+                      sql: "SELECT * FROM bgcv10beta_200m_wgs84",
+                                      cartocss: el.bec_cartocss.zone,
+                                      interactivity: "cartodb_id, map_label"
+                        }
+
+                  var sublayer = layer.getSubLayer(0);
+                  sublayer.set(subLayerOptions);
+                // el.data_layer.setCartoCSS(el.bec_cartocss.zone);
+
+                sublayer.setInteraction(true);
+
+                sublayer.on('featureOver', function(e, latlng, pos, data, subLayerIndex) {
                     el.selected_unit = data['map_label'];
                     highlightSelectedUnit();
                 }).on('featureOut', function(e, latlng, pos, data, layer) {
                     // console.log("out");
                 })
+
                 // cartodb tooltip overlay template
-            layer.leafletMap.viz.addOverlay({
-                type: 'tooltip',
-                layer: el.data_layer,
-                // template: '<div class="cartodb-tooltip-content-wrapper"><br>\
-                // <center><h5>{{map_label}}</h5></center><br></div>', 
-                template: '<p>{{map_label}}</p>',
-                position: 'top|left',
-                fields: [{ name: 'map_label' }]
+                layer.leafletMap.viz.addOverlay({
+                    type: 'tooltip',
+                    layer: el.data_layer,
+                    template: '<p>{{map_label}}</p>',
+                    position: 'top|left',
+                    fields: [{ name: 'map_label' }]
+                });
+
+
+            }).error(function(err) {
+                console.log("some error occurred: " + err);
             });
-
-
-        }).error(function(err) {
-            console.log("some error occurred: " + err);
-        });
     };
 
     var highlightSelectedUnit = function() {
